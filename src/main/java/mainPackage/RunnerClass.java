42377ebba9ab12110ca5eeb84a3bc5bebbb66bbd
gopivasala01 <gopi.v@beetlerim.com>
2024-01-03 07:52:17 -0500

    update

--- src/main/java/mainPackage/RunnerClass.java
+++ src/main/java/mainPackage/RunnerClass.java
@@ -221,7 +221,7 @@ public class RunnerClass
 			}
 			DataBase.updateTable(query);
 
-			 RunnerClass.driver.navigate().refresh();
+			 RunnerClass.driver.get(AppConfig.homeURL);
 		     Thread.sleep(2000);
 			
 			}
